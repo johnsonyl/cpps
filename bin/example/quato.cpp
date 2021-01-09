90c9478eef95f27c1a9ce640d0cfd7e9eac65352
Johnsonyl <88481106@qq.com>
2021-01-09 21:53:34 +0800

    Create quato.cpp
    新增引用示例

---
+++ bin/example/quato.cpp
@@ -0,0 +1,34 @@
+class Vec2D
+{
+	Vec2D(){
+
+	}
+
+	var& operator [](var key){
+		return xx[key];
+	}
+	
+	var xx = [1];
+}
+
+
+var p1 = new Vec2D();
+p1[0] = 1000;
+println(p1[0]);
+
+var &a = p1[0];
+a = 100;
+println(p1[0]);
+
+var set(var &v)
+{
+	v = 200;
+}
+set(a);
+set(p1[0]);
+
+println(a);
+println(p1[0]);
+
+var &b = p1;
+println(b[0]);
